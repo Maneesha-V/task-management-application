ed3245275055eda6cbf3c6e6b8a563c87618ce16
maneesha <manu.maneeshav@gmail.com>
2026-09-25 14:53:39 +0530

    Env changes in server.js

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -6,21 +6,15 @@ import app from "./app";
 import { connectDB } from "./config/db";
 import { initSocket } from "./socket/socket";
 
-const PORT = process.env.PORT || 5000;
+const PORT = Number(process.env.PORT) || 5000;
 
-// const start = async () => {
-//   await connectDB();
-//   app.listen(PORT, () => {
-//     console.log(`Server running on http://localhost:${PORT}`);
-//   });
-// };
 const start = async () => {
   await connectDB();
 
   const httpServer = http.createServer(app);
   initSocket(httpServer);
 
-  httpServer.listen(PORT, () => {
+  httpServer.listen(PORT, "0.0.0.0", () => {
     console.log(`Server running on http://localhost:${PORT}`);
   });
 };
